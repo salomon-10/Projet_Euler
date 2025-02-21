f1bf46ad2e0937bccfa5590d984e88be02403059
salomon-10 <sikasalomon4@gmail.com>
2025-02-21 01:06:28 +0000

    Update PythagoreanTriplet.c

--- PythagoreanTriplet.c
+++ PythagoreanTriplet.c
@@ -3,8 +3,9 @@
 
 void Triplet(){
 	int a,b,c,produit;
+	//parcour tous de 1 a 999
 	for(a=1;a<1000;a++){
-		for(b=a;b<1000-a;b++){
+		for(b=a;b<1000-a;b++){//puisque a<b<c  telq la som=1000 bpart de la valeur actuels de a  allant vers 1000-a(valeur actuels de a)
 			c=1000-a-b;
 			if(a*a+b*b==c*c){
 				printf("a=%d ; b=%d ; c=%d\n",a,b,c);
