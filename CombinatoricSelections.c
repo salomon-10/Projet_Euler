6258de7d08881301db67e00a11befeb0dfe63d3e
salomon-10 <sikasalomon4@gmail.com>
2025-03-07 10:23:20 +0000

    Update CombinatoricSelections.c

--- CombinatoricSelections.c
+++ CombinatoricSelections.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Fonction optimisée pour calculer C(n, r) sans overflow
+// Fonction  pour calculer C(n, r)  
 long long combine(int n, int r) {
-    if (r > n - r) r = n - r; // Exploite la symétrie C(n, r) = C(n, n-r)
+    if (r > n - r) r = n - r; // Exploite la symÃ©trie C(n, r) = C(n, n-r)
 int i;
     long long p = 1;
     for ( i = 0; i < r; i++) {
         p = p * (n - i) / (i + 1); 
-        if (p > 1000000) return p; // Arrêt anticipé si supérieur à 1M
+        if (p > 1000000) return p; // ArrÃªt si sup Ã  1M
     }
     return p;
 }
@@ -17,7 +17,7 @@ int main() {
     int n, r;
     int nb = 0;
 
-    //  valeurs distinctes supérieures à 1 million
+    //  valeurs distinctes supÃ©rieures Ã  1 million
     for (n = 1; n <= 100; n++) {
         for (r = 0; r <= n; r++) {
             if (combine(n, r) > 1000000) {
@@ -28,10 +28,9 @@ int main() {
 
     printf("Nombre de valeurs distinctes > a 1 million : %d\n", nb);
 
-    // Exemple d'affichage d'une combinaison spécifique
-//    n = 5, r = 2;
-//    printf("C(%d, %d) = %lld\n", n, r, combine(n, r));
+    //  affichage d'une combinaison spÃ©cifique
+	//n = 5, r = 2;
+	//printf("C(%d, %d) = %lld\n", n, r, combine(n, r));
 
     return 0;
 }
-
